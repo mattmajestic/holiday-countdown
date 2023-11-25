af5da71629a47d6b3262c99c41ee2c8c4797b1c3
Majestic Coding <33904170+mattmajestic@users.noreply.github.com>
2023-11-25 01:51:27 +0000

    ui app update

--- app/src/App.js
+++ app/src/App.js
@@ -2,41 +2,39 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [isChristmas, setIsChristmas] = useState(false);
+  const today = new Date();
+  const year = today.getFullYear();
+  const thanksgiving = new Date(year, 10, 23); // Thanksgiving is on November 23 this year
+  const christmas = new Date(year, 11, 25); // Christmas is always on December 25
+
+  const initialHoliday = today > thanksgiving && today < christmas ? 'Christmas' : 'Thanksgiving';
+  const [selectedHoliday, setSelectedHoliday] = useState(initialHoliday);
   const [daysUntil, setDaysUntil] = useState(0);
 
   useEffect(() => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const thanksgiving = new Date(year, 10, 23); // Thanksgiving is on November 23 this year
-    const christmas = new Date(year, 11, 25); // Christmas is always on December 25
-
-    if (today > thanksgiving && today < christmas) {
-      setIsChristmas(false);
-      setDaysUntil(Math.ceil((christmas - today) / (1000 * 60 * 60 * 24)));
-    } else if (today > christmas || today < thanksgiving) {
-      setIsChristmas(false);
-      const nextThanksgiving = today > christmas ? new Date(year + 1, 10, 23) : thanksgiving;
-      setDaysUntil(Math.ceil((nextThanksgiving - today) / (1000 * 60 * 60 * 24)));
-    } else if (today > thanksgiving && today < christmas) {
-      setIsChristmas(true);
-      setDaysUntil(Math.ceil((christmas - today) / (1000 * 60 * 60 * 24)));
+    let targetDate;
+    if (selectedHoliday === 'Thanksgiving') {
+      targetDate = today > thanksgiving ? new Date(year + 1, 10, 23) : thanksgiving;
+    } else {
+      targetDate = today > christmas ? new Date(year + 1, 11, 25) : christmas;
     }
-  }, []);
+
+    setDaysUntil(Math.ceil((targetDate - today) / (1000 * 60 * 60 * 24)));
+  }, [selectedHoliday]);
 
   return (
-    <div className={`app ${isChristmas ? "christmas" : "thanksgiving"}`}>
-      <h1>{isChristmas ? "It's Christmas!" : "It's Thanksgiving!"}</h1>
-      <p>There are {daysUntil} days until {isChristmas ? "Thanksgiving" : "Christmas"}</p>
+    <div className={`app ${selectedHoliday.toLowerCase()}`}>
+      <h1>{selectedHoliday} Countdown</h1>
+      <p>There are {daysUntil} days until {selectedHoliday}</p>
       <label>
-        <input type="radio" value="Thanksgiving" checked={!isChristmas} onChange={() => setIsChristmas(false)} /> 
+        <input type="radio" value="Thanksgiving" checked={selectedHoliday === 'Thanksgiving'} onChange={() => setSelectedHoliday('Thanksgiving')} /> 
         <span>Thanksgiving</span>
       </label>
       <label>
-        <input type="radio" value="Christmas" checked={isChristmas} onChange={() => setIsChristmas(true)} /> 
+        <input type="radio" value="Christmas" checked={selectedHoliday === 'Christmas'} onChange={() => setSelectedHoliday('Christmas')} /> 
         <span>Christmas</span>
       </label>
-      {!isChristmas ? (
+      {selectedHoliday === 'Thanksgiving' ? (
         <div>
           <iframe width="560" height="315" src="https://www.youtube.com/embed/CORgL-Iejps" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
